c6043cdc191b1423e17353333a61f6fdbae73618
sharktie <sakthikbala@gmail.com>
2026-04-02 11:21:25 +0100

    fixed assets

--- src/scenes/PreloadStuff.js
+++ src/scenes/PreloadStuff.js
@@ -5,19 +5,19 @@ export default class PreloadStuff extends Phaser.Scene {
   }
 
   preload() {
-     this.load.image('player', 'assets/player.png');   //player image
-     this.load.image('platform', 'assets/platform.png');    //platform image
-     this.load.image('bg', 'assets/bg.png');   //background image
-     this.load.image('coin', 'assets/coin.png'); //coin image
-     this.load.image('brick', 'assets/brick.png'); //brick image
-     this.load.image('enemy', 'assets/enemy.png'); //enemy image
+     this.load.image('player', 'Assets/player.png');   //player image
+     this.load.image('platform', 'Assets/platform.png');    //platform image
+     this.load.image('bg', 'Assets/bg.png');   //background image
+     this.load.image('coin', 'Assets/coin.png'); //coin image
+     this.load.image('brick', 'Assets/brick.png'); //brick image
+     this.load.image('enemy', 'Assets/enemy.png'); //enemy image
 
-     this.load.audio('menu', 'assets/menu.mp3');   //menu music
-     this.load.audio('type', 'assets/type.mp3');   //typewriter sound
-     this.load.audio('bgm', 'assets/bgm.mp3');     //background music
-     this.load.audio('jump', 'assets/jump.mp3');   //jump sound
-     this.load.audio('dash', 'assets/dash.mp3');   //dash sound
-     this.load.audio('death', 'assets/death.mp3'); //death sound
+     this.load.audio('menu', 'Assets/menu.mp3');   //menu music
+     this.load.audio('type', 'Assets/type.mp3');   //typewriter sound
+     this.load.audio('bgm', 'Assets/bgm.mp3');     //background music
+     this.load.audio('jump', 'Assets/jump.mp3');   //jump sound
+     this.load.audio('dash', 'Assets/dash.mp3');   //dash sound
+     this.load.audio('death', 'Assets/death.mp3'); //death sound
      }
 
   create() {
